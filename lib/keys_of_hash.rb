ca6153f4545596bbebef222ca35fb7fddc33e203
Jasmine Chen <jasmine1226@gmail.com>
2019-05-20 05:45:24 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,6 +3,7 @@ class Hash
     # code goes here
     keys = []
     arguments.each do |argument|
+      puts "#{self.index(argument)}: #{argument}"
       keys << self.index(argument)
       puts self.index(argument)
     end
