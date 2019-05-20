f7dca61d2bc17dd1191cc923ce3fbb12e885e2f9
Jasmine Chen <jasmine1226@gmail.com>
2019-05-20 05:51:22 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,7 @@ class Hash
     keys = []
     arguments.each do |argument|
       puts "#{self.select{|k,v| v == argument}}: #{argument}"
-      keys = self.select{|k,v| v == argument}.values
+      keys <<  self.select{|k,v| v == argument}.values
     end
   keys
   end
